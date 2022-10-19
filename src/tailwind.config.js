213551368297ab051cfa4358f3b38837fc10cd19
Hugo Da Silva <hugo.dasilva.sio94@gmail.com>
2022-10-19 11:24:04 +0200

    initial setup

---
+++ src/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: [
+    './resources/**/*.blade.php',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
