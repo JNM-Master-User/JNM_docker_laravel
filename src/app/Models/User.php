cb49e8f0ea389314199270e6233b0808c0a4701c
Hugo Da Silva <hugo.dasilva.filipe@gmail.com>
2022-10-25 12:49:42 +0200

    Users => name attribute deletion

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -47,4 +47,9 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
     public $incrementing = false;
+
+    public function userSensitiveData()
+    {
+        return $this->hasOne(UserSensitiveData::class);
+    }
 }
--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
+        <link rel="stylesheet">
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
