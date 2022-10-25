f8a641a0bfc71c4bab82954d4f3c4fb2fddbc38f
Hugo Da Silva <hugo.dasilva.filipe@gmail.com>
2022-10-25 12:45:59 +0200

    User and Admin routes transfer

--- src/routes/auth.php
+++ src/routes/auth.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Auth\EmailVerificationPromptController;
 use App\Http\Controllers\Auth\NewPasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\Auth\SaveUserSensitiveDataController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
