6b1c9f16acc3aa13c361701e1d89a975d1891a72
Hugo Da Silva <hugo.dasilva.filipe@gmail.com>
2022-10-25 12:46:42 +0200

    User and Admin routes integration

--- src/app/Providers/RouteServiceProvider.php
+++ src/app/Providers/RouteServiceProvider.php
@@ -35,6 +35,12 @@ class RouteServiceProvider extends ServiceProvider
 
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
+
+            Route::middleware('rights-user')
+                ->group(base_path('routes/rights-user.php'));
+
+            Route::middleware('rights-admin')
+                ->group(base_path('routes/rights-admin.php'));
         });
     }
 
---
+++ src/routes/rights-user.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Http\Controllers\Auth\SaveUserSensitiveDataController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| profil
+|--------------------------------------------------------------------------
+*/
+Route::middleware('rights-user')->group(function () {
+    Route::post('user-sensitive-data/save', [SaveUserSensitiveDataController::class, 'store'])
+        ->name('user.user-sensitive-data.save');
+
+    Route::get('user-sensitive-data/delete', [SaveUserSensitiveDataController::class, 'destroy'])
+        ->name('user.user-sensitive-data.destroy');
+});
