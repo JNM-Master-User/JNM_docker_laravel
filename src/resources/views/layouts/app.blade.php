1f4243fee0145e9df1ea122d7a9949d76b2f6f17
Hugo Da Silva <hugo.dasilva.filipe@gmail.com>
2022-10-31 00:45:18 +0100

    Font Awesome integration

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <script src="https://kit.fontawesome.com/03c1dbd5d5.js" crossorigin="anonymous"></script>
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
