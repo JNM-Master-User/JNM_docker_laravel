ff4f5032634574d9d9dad1158d877f2b81dbc715
Hugo Da Silva <hugo.dasilva.filipe@gmail.com>
2022-11-16 10:11:53 +0100

    FRONT route modification

--- src/routes/web.php
+++ src/routes/web.php
@@ -14,11 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.welcome');
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('pages.dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 require __DIR__.'/auth.php';
