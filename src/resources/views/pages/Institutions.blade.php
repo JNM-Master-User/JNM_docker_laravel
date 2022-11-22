36df42f997fb8add83e265a4485a013f36b8f2e2
Miyrage <40011387@parisnanterre.fr>
2022-11-22 14:27:33 +0100

    INSTITUTIONS CRUD Implementation

--- src/resources/views/pages/Institutions.blade.php
+++ src/resources/views/pages/Institutions.blade.php
@@ -1,3 +1,10 @@
 <x-app-layout>
+
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('roles') }}
+        </h2>
+    </x-slot>
+
     {{$institutions}}
 </x-app-layout>
