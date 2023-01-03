966db83d403da9099e6aa7501a2dd3b82170d41a
Miyrage <40011387@parisnanterre.fr>
2023-01-03 09:23:49 +0100

    FRONT BACK edi push inline

--- src/resources/views/pages/accueil.blade.php
+++ src/resources/views/pages/accueil.blade.php
@@ -25,12 +25,12 @@
         <div class="container px-3 mx-auto flex flex-wrap flex-col md:flex-row items-center">
             <!--Left Col-->
             <div class="flex flex-col w-full md:w-2/5 justify-center items-start text-center md:text-left">
-                <p class="uppercase tracking-loose w-full">What business are you?</p>
+                <p class="uppercase tracking-loose w-full">Welcome to the JNM site !</p>
                 <h1 class="my-4 text-5xl font-bold leading-tight">
-                    Main Hero Message to sell yourself!
+                    Bienvenue sur le site de la Journée Internationale de la MIAGE !
                 </h1>
                 <p class="leading-normal text-2xl mb-8">
-                    Sub-hero message, not too long and not too short. Make it just right!
+                    Vous trouverez ici toutes les informations relatives à la JNM et la MIAGE en général
                 </p>
                 <button class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
                     Subscribe
@@ -380,7 +380,7 @@
     <section class="bg-white border-b py-8">
         <div class="container mx-auto flex flex-wrap pt-4 pb-12">
             <h2 class="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">
-                Title
+                Licence et Master MIAGE
             </h2>
             <div class="w-full mb-4">
                 <div class="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"></div>
@@ -388,58 +388,41 @@
             <div class="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
                 <div class="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow">
                     <a href="#" class="flex flex-wrap no-underline hover:no-underline">
-                        <p class="w-full text-gray-600 text-xs md:text-sm px-6">
-                            xGETTING STARTED
-                        </p>
                         <div class="w-full font-bold text-xl text-gray-800 px-6">
-                            Lorem ipsum dolor sit amet.
+                            Licence MIASHS parcours MIAGE
                         </div>
                         <p class="text-gray-800 text-base px-6 mb-5">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at ipsum eu nunc commodo posuere et sit amet ligula.
+                            La Licence MIASHS est une licence pluridisciplinaire qui associe un tronc commun de mathématiques appliquées et d’informatique à une formation complémentaire en économie et en gestion.
+                            Dans cette formation, les étudiants apprennent à modéliser et traiter des problèmes issus du monde économique ou des systèmes d'information des entreprises.
                         </p>
                     </a>
-                </div>
-                <div class="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6">
-                    <div class="flex items-center justify-start">
-                        <button class="mx-auto lg:mx-0 hover:underline gradient text-white font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
-                            Action
-                        </button>
-                    </div>
                 </div>
             </div>
             <div class="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
                 <div class="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow">
                     <a href="#" class="flex flex-wrap no-underline hover:no-underline">
-                        <p class="w-full text-gray-600 text-xs md:text-sm px-6">
-                            xGETTING STARTED
-                        </p>
                         <div class="w-full font-bold text-xl text-gray-800 px-6">
-                            Lorem ipsum dolor sit amet.
+                            [Master] Méthodes informatiques appliquées à la gestion des entreprises (MIAGE)
                         </div>
                         <p class="text-gray-800 text-base px-6 mb-5">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at ipsum eu nunc commodo posuere et sit amet ligula.
+                            Le diplôme de Master Miage propose une double compétence et vise la formation des futurs acteurs du développement des organisations capables d’accompagner et de piloter les évolutions de la stratégie des entreprises et des évolutions technologiques numériques.
+                            Il se prépare en deux années avec deux formats possibles d’organisation des études : classique avec un stage en entreprise en fin de chacune des deux années ou apprentissage avec alternance de périodes à l’université et de périodes en entreprise tout au long de chacune des deux années.
                         </p>
                     </a>
-                </div>
-                <div class="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6">
-                    <div class="flex items-center justify-center">
-                        <button class="mx-auto lg:mx-0 hover:underline gradient text-white font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
-                            Action
-                        </button>
-                    </div>
                 </div>
             </div>
             <div class="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
                 <div class="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow">
                     <a href="#" class="flex flex-wrap no-underline hover:no-underline">
-                        <p class="w-full text-gray-600 text-xs md:text-sm px-6">
-                            xGETTING STARTED
-                        </p>
                         <div class="w-full font-bold text-xl text-gray-800 px-6">
-                            Lorem ipsum dolor sit amet.
+                            Et après ?
                         </div>
                         <p class="text-gray-800 text-base px-6 mb-5">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at ipsum eu nunc commodo posuere et sit amet ligula.
+                            Une fois diplomé d'une licence ou d'un master MIAGE, on peut travailler dans différent secteur :
+                            <br>- Les entreprises de services numériques
+                            <br>- Les banques, plus largement le secteur de la finance et les compagnies d’assurance
+                            <br>- Les entreprises de distribution, de transport, de tourisme
+                            <br> Et bien d'autres domaines encore.
                         </p>
                     </a>
                 </div>
@@ -571,23 +554,44 @@
         </button>
     </section>
     <!--Footer-->
-    <footer class="bg-white dark:bg-gray-700">
+    <footer class="bg-white">
         <div class="container mx-auto px-8">
             <div class="w-full flex flex-col md:flex-row py-6">
                 <div class="flex-1 mb-6 text-black">
-                    <a href="{{ route('accueil') }}" class="text-xl font-bold flex items-center lg:ml-2.5">
-                        <x-application-logo class="block fill-current text-gray-600 h-6 mr-2" >
-                        </x-application-logo>
+                    <a class="text-pink-600 no-underline hover:no-underline font-bold text-2xl lg:text-4xl" href="#">
+                        <!--Icon from: http://www.potlabicons.com/ -->
+                        <svg class="h-8 fill-current inline" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512.005 512.005">
+                            <rect fill="#2a2a31" x="16.539" y="425.626" width="479.767" height="50.502" transform="matrix(1,0,0,1,0,0)" />
+                            <path
+                                    class="plane-take-off"
+                                    d=" M 510.7 189.151 C 505.271 168.95 484.565 156.956 464.365 162.385 L 330.156 198.367 L 155.924 35.878 L 107.19 49.008 L 211.729 230.183 L 86.232 263.767 L 36.614 224.754 L 0 234.603 L 45.957 314.27 L 65.274 347.727 L 105.802 336.869 L 240.011 300.886 L 349.726 271.469 L 483.935 235.486 C 504.134 230.057 516.129 209.352 510.7 189.151 Z "
+                            />
+                        </svg>
+                        LANDING
                     </a>
+                </div>
+                <div class="flex-1">
+                    <p class="uppercase text-gray-500 md:mb-6">Links</p>
+                    <ul class="list-reset mb-6">
+                        <li class="mt-2 inline-block mr-2 md:block md:mr-0">
+                            <a href="#" class="no-underline hover:underline text-gray-800 hover:text-pink-500">FAQ</a>
+                        </li>
+                        <li class="mt-2 inline-block mr-2 md:block md:mr-0">
+                            <a href="#" class="no-underline hover:underline text-gray-800 hover:text-pink-500">Help</a>
+                        </li>
+                        <li class="mt-2 inline-block mr-2 md:block md:mr-0">
+                            <a href="#" class="no-underline hover:underline text-gray-800 hover:text-pink-500">Support</a>
+                        </li>
+                    </ul>
                 </div>
                 <div class="flex-1">
                     <p class="uppercase text-gray-500 md:mb-6">Legal</p>
                     <ul class="list-reset mb-6">
                         <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-                            <a href="#" class="no-underline hover:underline text-gray-800 dark:text-white hover:text-gray-200">Conditions</a>
+                            <a href="#" class="no-underline hover:underline text-gray-800 hover:text-pink-500">Terms</a>
                         </li>
                         <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-                            <a href="#" class="no-underline hover:underline text-gray-800 dark:text-white hover:text-gray-200">Cookies</a>
+                            <a href="#" class="no-underline hover:underline text-gray-800 hover:text-pink-500">Privacy</a>
                         </li>
                     </ul>
                 </div>
@@ -595,33 +599,33 @@
                     <p class="uppercase text-gray-500 md:mb-6">Social</p>
                     <ul class="list-reset mb-6">
                         <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-                            <a href="#" class="no-underline hover:underline text-gray-800 dark:text-white hover:text-gray-200">Facebook</a>
+                            <a href="#" class="no-underline hover:underline text-gray-800 hover:text-pink-500">Facebook</a>
                         </li>
                         <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-                            <a href="#" class="no-underline hover:underline text-gray-800 dark:text-white hover:text-gray-200">Linkedin</a>
+                            <a href="#" class="no-underline hover:underline text-gray-800 hover:text-pink-500">Linkedin</a>
                         </li>
                         <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-                            <a href="#" class="no-underline hover:underline text-gray-800 dark:text-white hover:text-gray-200">Twitter</a>
+                            <a href="#" class="no-underline hover:underline text-gray-800 hover:text-pink-500">Twitter</a>
                         </li>
                     </ul>
                 </div>
                 <div class="flex-1">
-                    <p class="uppercase text-gray-500 md:mb-6">JNM</p>
+                    <p class="uppercase text-gray-500 md:mb-6">Company</p>
                     <ul class="list-reset mb-6">
                         <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-                            <a href="#" class="no-underline hover:underline text-gray-800 dark:text-white hover:text-gray-200">Blog</a>
+                            <a href="#" class="no-underline hover:underline text-gray-800 hover:text-pink-500">Official Blog</a>
                         </li>
                         <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-                            <a href="#" class="no-underline hover:underline text-gray-800 dark:text-white hover:text-gray-200">À propos</a>
+                            <a href="#" class="no-underline hover:underline text-gray-800 hover:text-pink-500">About Us</a>
                         </li>
                         <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-                            <a href="#" class="no-underline hover:underline text-gray-800 dark:text-white hover:text-gray-200">Contact</a>
+                            <a href="#" class="no-underline hover:underline text-gray-800 hover:text-pink-500">Contact</a>
                         </li>
                     </ul>
                 </div>
             </div>
         </div>
-        <a href="https://www.freepik.com/free-photos-vectors/background" class="text-gray-500">JNM - 2022-2023 - Projet Web</a>
+        <a href="https://www.freepik.com/free-photos-vectors/background" class="text-gray-500">Background vector created by freepik - www.freepik.com</a>
     </footer>
     <!-- jQuery if you need it
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
