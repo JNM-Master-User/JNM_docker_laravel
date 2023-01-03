dddc28a95655783c9ac90315f1ba941a4a154263
Hugo Da Silva <hugo.dasilva.filipe@gmail.com>
2023-01-03 09:08:03 +0100

    FRONT-BACK push inline

--- src/resources/views/pages/accueil.blade.php
+++ src/resources/views/pages/accueil.blade.php
@@ -571,44 +571,23 @@
         </button>
     </section>
     <!--Footer-->
-    <footer class="bg-white">
+    <footer class="bg-white dark:bg-gray-700">
         <div class="container mx-auto px-8">
             <div class="w-full flex flex-col md:flex-row py-6">
                 <div class="flex-1 mb-6 text-black">
-                    <a class="text-pink-600 no-underline hover:no-underline font-bold text-2xl lg:text-4xl" href="#">
-                        <!--Icon from: http://www.potlabicons.com/ -->
-                        <svg class="h-8 fill-current inline" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512.005 512.005">
-                            <rect fill="#2a2a31" x="16.539" y="425.626" width="479.767" height="50.502" transform="matrix(1,0,0,1,0,0)" />
-                            <path
-                                    class="plane-take-off"
-                                    d=" M 510.7 189.151 C 505.271 168.95 484.565 156.956 464.365 162.385 L 330.156 198.367 L 155.924 35.878 L 107.19 49.008 L 211.729 230.183 L 86.232 263.767 L 36.614 224.754 L 0 234.603 L 45.957 314.27 L 65.274 347.727 L 105.802 336.869 L 240.011 300.886 L 349.726 271.469 L 483.935 235.486 C 504.134 230.057 516.129 209.352 510.7 189.151 Z "
-                            />
-                        </svg>
-                        LANDING
+                    <a href="{{ route('accueil') }}" class="text-xl font-bold flex items-center lg:ml-2.5">
+                        <x-application-logo class="block fill-current text-gray-600 h-6 mr-2" >
+                        </x-application-logo>
                     </a>
-                </div>
-                <div class="flex-1">
-                    <p class="uppercase text-gray-500 md:mb-6">Links</p>
-                    <ul class="list-reset mb-6">
-                        <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-                            <a href="#" class="no-underline hover:underline text-gray-800 hover:text-pink-500">FAQ</a>
-                        </li>
-                        <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-                            <a href="#" class="no-underline hover:underline text-gray-800 hover:text-pink-500">Help</a>
-                        </li>
-                        <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-                            <a href="#" class="no-underline hover:underline text-gray-800 hover:text-pink-500">Support</a>
-                        </li>
-                    </ul>
                 </div>
                 <div class="flex-1">
                     <p class="uppercase text-gray-500 md:mb-6">Legal</p>
                     <ul class="list-reset mb-6">
                         <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-                            <a href="#" class="no-underline hover:underline text-gray-800 hover:text-pink-500">Terms</a>
+                            <a href="#" class="no-underline hover:underline text-gray-800 dark:text-white hover:text-gray-200">Conditions</a>
                         </li>
                         <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-                            <a href="#" class="no-underline hover:underline text-gray-800 hover:text-pink-500">Privacy</a>
+                            <a href="#" class="no-underline hover:underline text-gray-800 dark:text-white hover:text-gray-200">Cookies</a>
                         </li>
                     </ul>
                 </div>
@@ -616,33 +595,33 @@
                     <p class="uppercase text-gray-500 md:mb-6">Social</p>
                     <ul class="list-reset mb-6">
                         <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-                            <a href="#" class="no-underline hover:underline text-gray-800 hover:text-pink-500">Facebook</a>
+                            <a href="#" class="no-underline hover:underline text-gray-800 dark:text-white hover:text-gray-200">Facebook</a>
                         </li>
                         <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-                            <a href="#" class="no-underline hover:underline text-gray-800 hover:text-pink-500">Linkedin</a>
+                            <a href="#" class="no-underline hover:underline text-gray-800 dark:text-white hover:text-gray-200">Linkedin</a>
                         </li>
                         <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-                            <a href="#" class="no-underline hover:underline text-gray-800 hover:text-pink-500">Twitter</a>
+                            <a href="#" class="no-underline hover:underline text-gray-800 dark:text-white hover:text-gray-200">Twitter</a>
                         </li>
                     </ul>
                 </div>
                 <div class="flex-1">
-                    <p class="uppercase text-gray-500 md:mb-6">Company</p>
+                    <p class="uppercase text-gray-500 md:mb-6">JNM</p>
                     <ul class="list-reset mb-6">
                         <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-                            <a href="#" class="no-underline hover:underline text-gray-800 hover:text-pink-500">Official Blog</a>
+                            <a href="#" class="no-underline hover:underline text-gray-800 dark:text-white hover:text-gray-200">Blog</a>
                         </li>
                         <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-                            <a href="#" class="no-underline hover:underline text-gray-800 hover:text-pink-500">About Us</a>
+                            <a href="#" class="no-underline hover:underline text-gray-800 dark:text-white hover:text-gray-200">À propos</a>
                         </li>
                         <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-                            <a href="#" class="no-underline hover:underline text-gray-800 hover:text-pink-500">Contact</a>
+                            <a href="#" class="no-underline hover:underline text-gray-800 dark:text-white hover:text-gray-200">Contact</a>
                         </li>
                     </ul>
                 </div>
             </div>
         </div>
-        <a href="https://www.freepik.com/free-photos-vectors/background" class="text-gray-500">Background vector created by freepik - www.freepik.com</a>
+        <a href="https://www.freepik.com/free-photos-vectors/background" class="text-gray-500">JNM - 2022-2023 - Projet Web</a>
     </footer>
     <!-- jQuery if you need it
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
